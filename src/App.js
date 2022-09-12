2dbb4237ca80586728626338989ebda5920b542e
ArtemRozzhivin <temchik200352@gmail.com>
2022-09-12 14:50:28 +0300

    users

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
@@ -6,12 +6,56 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [isInvated, setInvated] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err) => {
+        console.log(err);
+        alert('Ошибка в получение пользователей!');
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const changeSetSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const changeSetInvated = (id) => {
+    if (isInvated.includes(id)) {
+      setInvated((prev) => prev.filter((invateId) => invateId !== id));
+    } else {
+      setInvated((prev) => [...prev, id]);
+    }
+  };
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={isInvated.length} />
+      ) : (
+        <Users
+          setSuccess={setSuccess}
+          isInvated={isInvated}
+          changeSetInvated={changeSetInvated}
+          search={search}
+          changeSetSearch={changeSetSearch}
+          items={users}
+          isLoading={isLoading}
+        />
+      )}
     </div>
   );
 }
 
 export default App;
+
+// сделать поиск по имени и емейле пользователя
+// сделать подгрузку скелетона
+//
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,27 +2,64 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({
+  items,
+  isLoading,
+  search,
+  changeSetSearch,
+  isInvated,
+  changeSetInvated,
+  setSuccess,
+}) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input
+          value={search}
+          onChange={changeSetSearch}
+          type="text"
+          placeholder="Найти пользователя..."
+        />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
           <Skeleton />
           <Skeleton />
           <Skeleton />
+          <Skeleton />
+          <Skeleton />
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items
+            .filter((obj) => {
+              const fullName = obj.first_name + ' ' + obj.last_name;
+
+              return (
+                fullName.toLowerCase().includes(search.toLowerCase()) ||
+                obj.email.toLowerCase().includes(search.toLowerCase())
+              );
+            })
+            .map((obj) => (
+              <User
+                isInvated={isInvated.includes(obj.id)}
+                changeSetInvated={changeSetInvated}
+                key={obj.id}
+                {...obj}
+              />
+            ))}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {isInvated.length ? (
+        <button onClick={setSuccess} className="send-invite-btn">
+          Отправить приглашение
+        </button>
+      ) : (
+        ''
+      )}
     </>
   );
 };
